4f0ff726b75d2169fefe6f9159488dade6b9e1b2
k.zhdanov <k.zhdanov@credinform.ru>
2016-01-14 11:51:27 +0300

    Изменен механиз рендера

--- Controllers/Home.js
+++ Controllers/Home.js
@@ -1,14 +1,22 @@
 var Actions = {};
 Actions.Index = function(req, res) {
 	if(req.query.error) {
-		return res.render('index', { 'isAuth': false, 'ErrorMsg' : 'Ошибка авторизации' });
+	return res.render('index', { tasks:  "Тело документа" }, function (err, html) {
+				 		if(err) throw err;
+				 		res.render('layout', { body: html, 'isAuth': false, 'ErrorMsg' : 'Ошибка авторизации'});
+				 	}
+				);
 	}
 
 	var login = "Unauthorized";
 	if(req && req.user)
 		login = req.user
 
-	res.render('index', { 'isAuth': req.isAuthenticated(), 'login': login });
+	res.render('index', { tasks:  "Тело документа" }, function (err, html) {
+				 		if(err) throw err;
+				 		res.render('layout', { body: html, 'isAuth': req.isAuthenticated(), 'login': login });
+				 	}
+				);
 },
 Actions.LogOut = function(req, res) {
 	req.logout();
